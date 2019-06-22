b40bb39cb31b3d613b7286829f9bf1c7075929fa
Chloe Dawn <admin@designdevils.co.uk>
2019-06-22 15:51:11 +0100

    Add support for Quark's wooden pressure plates
    Resolves #2

--- src/main/java/net/insomniakitten/plated/compat/quark/CompatQuark.java
+++ src/main/java/net/insomniakitten/plated/compat/quark/CompatQuark.java
@@ -15,6 +15,11 @@ import net.minecraftforge.registries.IForgeRegistry;
 @ObjectHolder("quark")
 public final class CompatQuark {
     public static final Block OBSIDIAN_PRESSURE_PLATE = Blocks.AIR;
+    public static final Block SPRUCE_PRESSURE_PLATE = Blocks.AIR;
+    public static final Block BIRCH_PRESSURE_PLATE = Blocks.AIR;
+    public static final Block JUNGLE_PRESSURE_PLATE = Blocks.AIR;
+    public static final Block ACACIA_PRESSURE_PLATE = Blocks.AIR;
+    public static final Block DARK_OAK_PRESSURE_PLATE = Blocks.AIR;
 
     private CompatQuark() {}
 
@@ -22,12 +27,22 @@ public final class CompatQuark {
     public static void onRegisterBlocks(final RegistryEvent.Register<Block> event) {
         final IForgeRegistry<Block> registry = event.getRegistry();
         registry.register(new BlockPressurePlatePlatedObsidian("obsidian_pressure_plate"));
+        registry.register(new BlockPressurePlatePlatedWooden("spruce_pressure_plate"));
+        registry.register(new BlockPressurePlatePlatedWooden("birch_pressure_plate"));
+        registry.register(new BlockPressurePlatePlatedWooden("jungle_pressure_plate"));
+        registry.register(new BlockPressurePlatePlatedWooden("acacia_pressure_plate"));
+        registry.register(new BlockPressurePlatePlatedWooden("dark_oak_pressure_plate"));
     }
 
     @SideOnly(Side.CLIENT)
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onRegisterModels(final ModelRegistryEvent event) {
         StateMapperPlated.registerFor(CompatQuark.OBSIDIAN_PRESSURE_PLATE);
+        StateMapperPlated.registerFor(CompatQuark.SPRUCE_PRESSURE_PLATE);
+        StateMapperPlated.registerFor(CompatQuark.BIRCH_PRESSURE_PLATE);
+        StateMapperPlated.registerFor(CompatQuark.JUNGLE_PRESSURE_PLATE);
+        StateMapperPlated.registerFor(CompatQuark.ACACIA_PRESSURE_PLATE);
+        StateMapperPlated.registerFor(CompatQuark.DARK_OAK_PRESSURE_PLATE);
     }
 
 }
